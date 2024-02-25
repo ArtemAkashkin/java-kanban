c7c6f938bc4587f0515adaae7617736d36b147ec
Artem Akashkin <artem_3233@mail.ru>
2024-02-25 16:08:09 +0300

    Улучшил чек стайл 3

--- src/service/Node.java
+++ src/service/Node.java
@@ -7,7 +7,7 @@ public class Node {
     public Node next;
     public Node prev;
 
-    public Node(Node prev,Task task, Node next) {
+    public Node(Node prev, Task task, Node next) {
         this.task = task;
         this.next = next;
         this.prev = prev;

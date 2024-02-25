fc396e14c0a37f338e4300013078d2a4a5366ca9
Artem Akashkin <artem_3233@mail.ru>
2024-02-25 12:09:39 +0300

    Сделал 6 проект, 1 часть

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -2,27 +2,59 @@ package service;
 
 import model.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    public LinkedList<Task> historyStorage;
-    private final int maxAmountOfStorage = 10;
+//    public LinkedList<Task> historyStorage;
+    private HashMap<Integer, Node> historyMap = new HashMap<>();
+    private List<Node> nodeListTasks; 
+
+
+    private Node first;
+
+    private Node last;
+
+    public void linkLast(Task lastNode) {
+        Node l = last;
+        Node newNode = new Node(l, lastNode, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        nodeListTasks.add(newNode);
+    }
+
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> getTasks = new ArrayList<>();
+        for (Node task : nodeListTasks) {
+            getTasks.add(task.task);
+        }
+        return getTasks;
+    }
+
+    @Override
+    public void removeNode(Node node) {
+        nodeListTasks.remove(node);
+    }
+
 
     public InMemoryHistoryManager() {
-        this.historyStorage = new LinkedList<>();
+        this.nodeListTasks = new LinkedList<>();
     }
 
     @Override
     public void add(Task task) {
-        if (historyStorage.size() >= maxAmountOfStorage) {
-            historyStorage.remove(0);
+        if (getTasks().contains(task)) {
+            removeNode(historyMap.get(task.getId()));
         }
-        historyStorage.add(task);
+        linkLast(task);
+        historyMap.put(task.getId(), last);
     }
 
     @Override
-    public LinkedList<Task> getHistory() {
-        return historyStorage;
+    public void remove(int id) {
+
     }
+
 }
